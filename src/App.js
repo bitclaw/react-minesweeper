ecb2dfb969835d2c64c0848c797936e07f714e4f
Daniel C <bitclaw@gmail.com>
2018-06-09 03:34:54 -0500

    Initial board setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import minesweeperIcon from './minesweeper.svg';
 import './App.css';
+import Board from './Board.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={minesweeperIcon} className="App-logo" alt="logo" />
-          <h1 className="App-title">Minesweeper</h1>
-        </header>
-        <p className="App-intro">
+    constructor (props) {
+        super(props);
 
-        </p>
-      </div>
-    );
-  }
+        this.state = {
+            height: 8,
+            width: 8,
+            mines: 7,
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={minesweeperIcon} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Minesweeper</h1>
+                </header>
+                <div className="minesweeper">
+                    <Board height={this.state.height} width={this.state.width} mines={this.state.mines} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
