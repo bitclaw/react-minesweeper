0f6ace44e05d81639b37ba830e8b43632227cdf9
Daniel C <bitclaw@gmail.com>
2018-06-11 01:48:50 -0500

    Import header and tiles component and import provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import minesweeperIcon from './minesweeper.svg';
 import './App.css';
-import Board from './Board.js'
+import Header from './components/Header'
+import Tiles from './components/Tiles.js'
 
 class App extends Component {
-    constructor (props) {
-        super(props);
-
-        this.state = {
-            height: 8,
-            width: 8,
-            mines: 7,
-        }
-    }
-
     render() {
         return (
             <div className="App">
@@ -21,8 +12,8 @@ class App extends Component {
                     <img src={minesweeperIcon} className="App-logo" alt="logo" />
                     <h1 className="App-title">Minesweeper</h1>
                 </header>
-                <div className="minesweeper">
-                    <Board height={this.state.height} width={this.state.width} mines={this.state.mines} />
+                <div className="Minesweeper-app">
+                    <div className="separator" />
                 </div>
             </div>
         );
